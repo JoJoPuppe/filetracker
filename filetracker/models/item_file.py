b08b7ed9401ff5f66362406af6910ee964204b34
Marcus Loeper <marcusloeper@gmx.de>
2022-11-27 18:44:30 +0100

    item model update and item route update

--- filetracker/models/item_file.py
+++ filetracker/models/item_file.py
@@ -1,8 +1,10 @@
 from datetime import datetime
-from pydantic import BaseModel, Field
-from .pyobjectid import PyObjectId
+from typing import List, Optional
+
 from bson import ObjectId
-from typing import Optional, List
+from pydantic import BaseModel, Field
+
+from .pyobjectid import PyObjectId
 
 
 class ItemFile(BaseModel):
@@ -19,21 +21,23 @@ class ItemFile(BaseModel):
     operator: str
     comment: str
     parent: Optional[PyObjectId]
+    history: Optional[List[PyObjectId]]
+    requirements: Optional[List[PyObjectId]]
 
     class Config:
         allow_population_by_field_name = True
         arbitrary_types_allowed = True
         json_encoders = {ObjectId: str}
         schema_extra = {
-                "example": {
-                    "path": "/new/path/to/file",
-                    "name": "My Item File",
-                    "file_name": "221022_my_item_file.ext",
-                    "status": "0",
-                    "project_id": "1923801231023",
-                    "file_type": "text",
-                    "creation_date": "2008-09-15T15:57:00+05:00",
-                    "operator": "jojo",
-                    "comment": "this is a new item"
-                    }
-                }
+            "example": {
+                "path": "/new/path/to/file",
+                "name": "My Item File",
+                "file_name": "221022_my_item_file.ext",
+                "status": "0",
+                "project_id": "1923801231023",
+                "file_type": "text",
+                "creation_date": "2008-09-15T15:57:00+05:00",
+                "operator": "jojo",
+                "comment": "this is a new item",
+            }
+        }
