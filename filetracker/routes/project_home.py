c2ea7dffb4b73a865945181ffc8e404d502969c0
jojopuppe <marcusloeper@gmx.de>
2022-12-01 18:21:56 +0100

    request correct order first pass

--- filetracker/routes/project_home.py
+++ filetracker/routes/project_home.py
@@ -4,9 +4,23 @@ from fastapi.encoders import jsonable_encoder
 from fastapi.responses import JSONResponse
 from filetracker.models.project_home import ProjectHome
 from ..tree.tree import tree
-from json import dumps
+from json import dumps, loads
 
 router = APIRouter()
+
+
+def build_flat_order(arr, no_parent):
+    x = {}
+    for i_idx, i in enumerate(arr):
+        if 'children' in i:
+            for j_idx, j in enumerate(i['children']):
+                x[j['_id']] = {"parent": i['_id'], "order": j_idx}
+
+                if 'children' in j:
+                    x = {**x, **build_flat_order(i['children'], False)}
+        if no_parent:
+            x[i['_id']] = {"parent": None, "order": i_idx}
+    return x
 
 @router.post("/", response_description="Project added to the database", response_model=ProjectHome)
 async def add_project(request: Request, project: ProjectHome = Body(...)):
@@ -18,9 +32,23 @@ async def add_project(request: Request, project: ProjectHome = Body(...)):
 
 @router.get("/", response_description="get all project", response_model=List[ProjectHome])
 async def list_projects(request: Request):
-    project = await request.app.database["project_home"].find().to_list(length=100)
+    project = await request.app.database["project_home"].find().to_list(length=300)
 
     return project
+
+@router.put("/reorder", response_description="reorder items")
+async def reorder_project(request: Request, all_items = Body(...)):
+    all_items = loads(all_items)
+    order_dict = build_flat_order(all_items, True)
+
+    get_project = await request.app.database['items'].find({"project_id": all_items[0]["project_id"]}).to_list(length=300)
+    if get_project is not None:
+        for item in get_project:
+            if order_dict[item['_id']]['parent'] != item['parent'] or order_dict[item['_id']]['order'] != item['order_index']:
+                item_file = {"parent": order_dict[item['_id']]['parent'],
+                    "order_index": order_dict[item['_id']]['order']}
+                await request.app.database["items"].update_one(
+                    {"_id": item['_id']}, {"$set": item_file})
 
 
 @router.get("/{proj_id}", response_description="get project by id")
@@ -33,7 +61,6 @@ async def find_project(proj_id: str, request: Request):
         {"$replaceRoot": { "newRoot": "$doc"}},
     ])
     project = await cursor.to_list(length=None)
-    print(project)
     #if (project := await request.app.database["items"].find({"project_id": proj_id }).to_list(length=300)) is not None:
     if project is not None:
 
@@ -53,12 +80,14 @@ async def find_project(proj_id: str, request: Request):
               ( A
               , parent
               , lambda node, children:
-                  dict([*list(node.items()), ("children", children(id(node)))])
+                dict([*list(node.items()), ("children", children(id(node)))])
               )
 
-            return dumps(C)
+            print(C)
+
+            return dumps({"tree": C, "flat": project})
         else:
-            return dumps([])
+            return dumps({"tree": [], "flat": []})
 
     raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Project with ID {proj_id} not found")
 
